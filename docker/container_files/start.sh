ca979878662d795ddce0a31304ed059f20c3d95a
rb2nem <rb2nem@gmail.com>
2017-04-17 19:43:07 +0200

    mitm in the container

--- docker/container_files/start.sh
+++ docker/container_files/start.sh
@@ -14,10 +14,6 @@ if [[ ! -f /opt/nem/package/nis/config-user.properties ]] ; then
 	EOF
 fi
 
-if [[ $# -gt 0 ]] && [[ $1 == "--no-nis" ]]; then
-  with_nis=0
-  shift
-fi
 
 # set owner nem, in case it is mounted from the host
 chown -R nem /var/lib/nem
@@ -30,17 +26,31 @@ Here are custom commands that are recognised. You can start launch them with
 the run.sh helper, with docker run, or from the shell in the container.
 
 - repl.js : open the node repl with nem-sdk loaded under the nem object.
+- mitm    : execute mitmproxy. This lets you inspect requests you send to it.
+            It is available on port 7892
 
 You can also pass options to the docker entrypoint:
 
 --no-nis : do not start nis
 
+The container also has a mitmweb interface available on port 8081.
+
+Ports to which you can send your REST queries (on the host or in the container):
+
+- 7890 : contact you local NIS directly
+- 7891 : send the request to mitmweb, which will pass it to your local NIS.
+         Inspect requests at http://localhost:8081
+- 7892 : send the request to mitmproxy, which will pass it to you local NIS.
+         You must first have started mitmproxy by accessing its terminal interface
+         by running `run.sh mitm`
+
 ###############################################################################
 
 EOF
 
+
 if [[ $# -eq 0 ]] ; then
-	[[ $with_nis -eq 1 ]] && exec /usr/bin/supervisord -c /etc/supervisord.conf -n
+        exec /usr/bin/supervisord -c /etc/supervisord.conf -n
 else
 	[[ $with_nis -eq 1 ]] && /usr/bin/supervisord -c /etc/supervisord.conf
 	exec "$@"
--- docker/run.sh
+++ docker/run.sh
@@ -9,6 +9,13 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P )"
 # go to script directory to have Dockerfile available
 cd $script_dir
 
+# check start flag
+with_nis=1
+if [[ $# -gt 0 ]] && [[ $1 == "--no-nis" ]]; then
+  with_nis=0
+  shift
+fi
+
 # we store the persistent data location for ease
 if [[ -f $script_dir/settings.sh ]]; then 
   . $script_dir/settings.sh
@@ -26,4 +33,14 @@ fi
 docker build -t nemdev .
 
 # run container
-docker run -it --rm -v $persistent_location:/var/lib/nem -p 7890:7890 nemdev "$@"
+# 7890: nis
+# 7891: mitmweb
+# 7892: mitmproxy console
+
+if docker ps --format '{{.Names}}' | grep nemdev>/dev/null ; then
+  # container is running, do an exec
+  # bash -i to get an interactive shell, and aliases loaded
+  docker exec -it nemdev bash -i -c "${@:-bash}"
+else
+  docker run -it --rm -v $persistent_location:/var/lib/nem -p 8081:8081 -p 7890:7890 -p 7891:7891 -p 7892:7892 --name nemdev nemdev bash -c "[[ $with_nis -eq 1 ]] && supervisorctl start nis ; ${@:-bash}"
+fi
