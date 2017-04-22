ec1bd28de21f222987dd4ebe6f106b145beffc10
rb2nem <rb2nem@gmail.com>
2017-04-22 18:08:38 +0200

    switch to docker-compose

---
+++ docker/mitm/container_files/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -eu
+
+if [[ $# -eq 0 ]] ; then
+        exec /usr/bin/supervisord -c /etc/supervisord.conf -n
+else
+	/usr/bin/supervisord -c /etc/supervisord.conf
+	exec "$@"
+fi
+
--- docker/run.sh
+++ docker/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+set -x
 # script to run the dev guide's docker image.
 # creates a settings.sh file where the path of directory holding the persistent data
 # accepts --no-nis as first argument to not start nis in the container
@@ -26,21 +27,50 @@ else
     echo "In which *existing* directory should the persistent data of the container be stored? (it should exist, no directory will be created)"
     read persistent_location
   done
-  echo "persistent_location=\"$persistent_location\"" > settings.sh
+  echo "#if you change this, change also mitp/.env!\nexport persistent_location=\"$persistent_location\"" > settings.sh
 fi
 
-# build image everytime, fast if no update needed
-docker build -t nemdev .
+containers=$(docker ps --format '{{.Names}}')
+nis_container=$(echo $containers | grep nis)
+mitm_container=$(echo $containers | grep mitm)
 
-# run container
-# 7890: nis
-# 7891: mitmweb
-# 7892: mitmproxy console
-
-if docker ps --format '{{.Names}}' | grep nemdev>/dev/null ; then
-  # container is running, do an exec
-  # bash -i to get an interactive shell, and aliases loaded
-  docker exec -it nemdev bash -i -c "${@:-bash}"
-else
-  docker run -it --rm -v $persistent_location:/var/lib/nem -p 8081:8081 -p 7890:7890 -p 7891:7891 -p 7892:7892 --name nemdev nemdev bash -c "[[ $with_nis -eq 1 ]] && supervisorctl start nis ; ${@:-bash}"
+# start docker containers if needed, possibly with nis
+if [[ -z "$nis_container" ]] ; then 
+  docker-compose up -d
+  [[ $with_nis -eq 1 ]] && docker exec -it docker_nemdevnis_1 supervisorctl start nis ;
 fi
+
+cat <<EOF
+
+
+###############################################################################
+Welcome to the NEM developer's guide Docker containers.
+Two containers have been started, one running NIS, and one running
+mitmproxy.
+
+Here are custom commands that are recognised. You can start launch them with 
+the run.sh helper, with docker run, or from the shell in the container.
+
+- repl.js : open the node repl in the nis container, with nem-sdk loaded under the nem object.
+- mitm    : execute mitmproxy. This lets you inspect requests you send to it.
+            It listens on your hosts's localhost interface on port 7890 and proxies your
+            request to NIS
+
+You can also pass options to the docker entrypoint:
+
+--no-nis : do not start nis
+
+The container also has a mitmweb interface available on port 8081.
+
+Ports to which you can send your REST queries (on the host or in the container):
+
+- 7890 : contact you local NIS directly
+- 7891 : send the request to mitmweb, which will pass it to your local NIS.
+         Inspect requests at http://localhost:8081
+- 7892 : send the request to mitmproxy, which will pass it to you local NIS.
+         You must first have started mitmproxy by accessing its terminal interface
+         by running 'run.sh mitm'
+
+###############################################################################
+
+EOF
