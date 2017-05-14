d906cc7529be3db060ab1487e9082eabd778a248
rb2nem <rb2nem@gmail.com>
2017-05-14 20:02:31 +0200

    sockjs and stomp in repl.js, new ndev --pull

--- docker/tools/container_files/repl.js
+++ docker/tools/container_files/repl.js
@@ -1,3 +1,5 @@
 #!/usr/local/bin/node
 global.nem = require('nem-sdk').default
+global.sockjs = require('sockjs-client').default
+global.stomp = require('stompjs').default
 global.repl = require('repl').start()
